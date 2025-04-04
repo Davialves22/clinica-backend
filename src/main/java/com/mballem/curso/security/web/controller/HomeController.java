8eb68dcd452e9a1a526ae2a1a48f5fbfabbab0c6
davi gerson <dgas1@discente.ifpe.edu.br>
2025-04-04 14:35:03 -0300

    adicionado back end da aplicação

---
+++ src/main/java/com/mballem/curso/security/web/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.mballem.curso.security.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	// retornar JSON
+	@GetMapping("/")
+	public ResponseEntity<Object> home() {
+
+		return ResponseEntity.ok().body(new Object() {
+			public String message = "Bem-vindo à home!";
+		});
+	}
+}
