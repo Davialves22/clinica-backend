a6d9fcb0020c608267e51d6e5a479bca44a57c2b
davi gerson <dgas1@discente.ifpe.edu.br>
2025-04-17 19:06:42 -0300

    alterações feitas no login

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -1,19 +1,40 @@
 package com.mballem.curso.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.web.servlet.config.annotation.*;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors()
+                .cors() // habilitar o CORS
                 .and()
+                .csrf().disable() // desativa CSRF usando o fetch()
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/", "/home", "/login", "/login-error", "/image/**", "/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login") // recebe POST do React
+                .successHandler((request, response, authentication) -> {
+                    response.setStatus(200); // devolve OK pro React
+                })
+                .failureHandler((request, response, exception) -> {
+                    response.sendError(401, "Credenciais inválidas"); // devolve erro para o React
+                })
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    response.setStatus(200); // avisa sucesso no logout
+                });
     }
+
+
 }
